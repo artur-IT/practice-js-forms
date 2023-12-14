57f1a2586218191069e4e97e88965898baaf0f76
Artur Matysiak <inz.matys@gmail.com>
2023-12-14 09:04:43 +0100

    task done

--- 04/app.js
+++ 04/app.js
@@ -1,40 +1,53 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
+const inputColor = document.querySelector("input[type=color]");
+const inputRange = document.querySelector("input[type=range]");
+const boxElement = document.querySelector(".box");
+
+//----------------
 function init() {
-    const boxElement = document.querySelector('.box');
-    setBoxShadow(boxElement, '#000000');
+  const boxElement = document.querySelector(".box");
+  setBoxShadow(boxElement, "#000000", (opacity = 1));
 }
 
 function setBoxShadow(element, colorInHex, opacity = 1) {
-    const colorInRGBA = `rgba(
-        ${getChannelColor(colorInHex, 'red')}, 
-        ${getChannelColor(colorInHex, 'green')}, 
-        ${getChannelColor(colorInHex, 'blue')}, 
+  const colorInRGBA = `rgba(
+        ${getChannelColor(colorInHex, "red")}, 
+        ${getChannelColor(colorInHex, "green")}, 
+        ${getChannelColor(colorInHex, "blue")}, 
         ${opacity}
     )`;
 
-    element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`;
+  return (element.style.boxShadow = `0 0 5px 5px ${colorInRGBA}`);
 }
-
 
 function getChannelColor(colorInHex, channelName) {
-    let start;
-    switch(channelName) {
-        case 'red':
-            start = 1;
-            break;
-        case 'green':
-            start = 3;
-            break;
-        case 'blue':
-            start = 5;
-            break;
-    }
+  let start;
+  switch (channelName) {
+    case "red":
+      start = 1;
+      break;
+    case "green":
+      start = 3;
+      break;
+    case "blue":
+      start = 5;
+      break;
+  }
 
-    const channelColorHex = colorInHex.substr(start, 2);
-    const channelColorDec = parseInt(channelColorHex, 16);
+  const channelColorHex = colorInHex.substr(start, 2);
+  const channelColorDec = parseInt(channelColorHex, 16);
 
-    return channelColorDec; 
+  return channelColorDec;
 }
+//------------------------
 
+inputRange.addEventListener("mousemove", (e) => {
+  const range = e.target.value;
+  setBoxShadow(boxElement, inputColor.value, range / 100);
+});
 
+inputColor.addEventListener("change", (e) => {
+  const color = e.target.value;
+  setBoxShadow(boxElement, color, inputRange.value / 100);
+});
